94a357dbc560f100e04ace888f34f50778a2cb3e
davidovski <david@davidovski.xyz>
2023-05-14 20:06:24 +0000

    Fix typo in slock

--- slock/deploy.sh
+++ slock/deploy.sh
@@ -5,7 +5,7 @@ pkgver=1.5
 [ -d "~/.local/src" ] && mkdir -p ~/.local/src
 
 curl -o ~/.local/src/slock-$pkgver.tar.gz https://dl.suckless.org/tools/slock-$pkgver.tar.gz 
-tar -C ~/.local/src/ -xvf ~/local/src/slock-$pkgver.tar.gz
+tar -C ~/.local/src/ -xvf ~/.local/src/slock-$pkgver.tar.gz
 
 srcdir=~/.local/src/slock-$pkgver
     cp config.h $srcdir

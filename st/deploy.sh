c9ffe81dc06e6de41d1fe7b81146fcbe42474751
davidovski <david@davidovski.xyz>
2023-07-16 01:54:27 +0100

    update st to full patch

--- st/deploy.sh
+++ st/deploy.sh
@@ -4,9 +4,10 @@ wd="$HOME/.local/src/st"
 
 [ ! -d "$HOME/.local/src" ] && mkdir -p "$wd"
 
+rm -rf "$wd"
 git clone https://git.suckless.org/st "$wd"
 
-patch -d "$wd" -p1 -i davidovski.patch
+patch -d "$wd" -p1 -i $(realpath davidovski.patch)
 
 make -C "$wd" 
 doas make -C "$wd" install PREFIX=/usr
